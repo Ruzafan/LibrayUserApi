d633b7fc8b5f4650c3e8fb257d1effed84f710f6
Ruzafan <marcramiro@gmail.com>
2025-04-30 18:30:30 +0200

    fix

--- UsersApi/Features/AddFriend/V1/Handler.cs
+++ UsersApi/Features/AddFriend/V1/Handler.cs
@@ -8,7 +8,7 @@ public class Handler(IRepository<User> userRepository, IRepository<FriendRequest
 {
     public async Task<bool> Handle(string userId, string friend, CancellationToken cancellationToken = default)
     {
-        var filter = Builders<User>.Filter.And(
+        var filter = Builders<User>.Filter.Or(
             Builders<User>.Filter.Eq(u => u.Id, userId),
             Builders<User>.Filter.Eq(u => u.Username, friend));
         var users = await userRepository.QueryItems(filter, cancellationToken);
